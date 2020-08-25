8a5f1567bc2e5aa9076a93039096c6cd0786e453
Jayabal Manoharan <mjayabalpsg@gmail.com>
2020-08-25 13:24:41 +0530

    Update Main.java

--- ThreadLocal/src/main/java/test/my/sample/Main.java
+++ ThreadLocal/src/main/java/test/my/sample/Main.java
@@ -1,1 +1,20 @@
 
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author jayabal.manoharan
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		ProcessingClass p = new ProcessingClass();
+		MyThread t1 = new MyThread(p);
+		t1.start();
+		MyThread t2 = new MyThread(p);
+		t2.start();
+	}
+}
